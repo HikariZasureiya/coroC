8553487d5a000e3511cd3b9969fe9f4a49d626a3
HikariZasureiya <info.d.podder@gmail.com>
2025-04-27 05:35:36 +0530

    added a way to send function argument address and return address to the coroutine function

--- coroutinesx86_64.h
+++ coroutinesx86_64.h
@@ -3,23 +3,11 @@
 #include<stdlib.h>
 #include<unistd.h>
 #define COR_NUM 100
+#define STACK_SIZE 4096
 
-
-
-typedef struct{
-    uint64_t rsp;
-    uint64_t rbp;
-    uint64_t rip;
-    uint64_t stackptr;
-} cor_pointers;
-  
-  
-typedef struct cor_struct{          // need this for coroutines
-    cor_pointers * corpointers;
-}cor_struct;
-
+// asm functions
 extern void coroutines_init(void * pointer);
-extern void coroutines_register(void * funptr , uintptr_t top , void* pointer);
+extern void coroutines_register(void * funptr , uintptr_t top , void* pointer , void * inarg , void * outarg);
 extern void coroutine_yield(void * basepointer);
 extern int  get_curr_cor_idx();
 extern void set_curr_cor_idx(uint64_t value);
@@ -27,8 +15,22 @@ extern int  get_cor_num();
 extern void set_cor_num(uint64_t value);
 extern void coroutine_kill(void * pointer);
 
+// a structure to store the essentials which switching stack. nothing much to see 
+typedef struct{
+    uint64_t rsp;
+    uint64_t rbp;
+    uint64_t rip;
+    uint64_t stackptr;
+    uint64_t inaddr;
+    uint64_t outaddr;
+} cor_pointers;
+  
+// could've just used a pointer to coroutines but nah a struct works too
+typedef struct cor_struct{          
+    cor_pointers * corpointers;
+}cor_struct;
 
-
+// memory allocator for cor_struct
 cor_struct* cor_struct_init(){
     cor_struct * newcor = (cor_struct *)malloc(sizeof(cor_struct));
     cor_pointers * newptr = (cor_pointers *)calloc(COR_NUM , sizeof(cor_pointers));
@@ -40,26 +42,47 @@ cor_struct* cor_struct_init(){
     return newcor;
 }
 
-
-void register_coroutines(void * funptr , cor_struct * corstruct){
-    uint64_t *stack = (uint64_t *)malloc(4096+16);
-    
+// registers function to be used as coroutine , allocates stack , takes in inaddr , outaddr
+void register_coroutines(void * funptr , cor_struct * corstruct , void * inarg , void * outarg){    
+    uint64_t *stack = (uint64_t *)malloc(STACK_SIZE+16);
     uintptr_t aligned = ((uintptr_t)stack + 15) & ~0xF;
     uintptr_t stack_top = aligned + 4096;
     int number = get_cor_num();
-    coroutines_register(funptr , stack_top , &(corstruct->corpointers)[number]);
+    coroutines_register(funptr , stack_top , &(corstruct->corpointers)[number] , inarg , outarg);
     corstruct->corpointers[number].stackptr = (uint64_t)(uintptr_t)stack;
+    corstruct->corpointers[number].inaddr = (uint64_t)(uintptr_t)inarg;
+    corstruct->corpointers[number].outaddr = (uint64_t)(uintptr_t)outarg;
 }
 
-void cor_kill(cor_struct * corstruct){
+  // retrieves input argument address
+void * retrieve_args(cor_struct * corstruct){                             
+    int curidx = get_curr_cor_idx();
+    void * ret = (void *)(uintptr_t)(corstruct->corpointers)[curidx].inaddr;
+    return ret;
+}
+
+//retrieves the output argument address
+void * retcor_val(cor_struct * corstruct){             
+    int curidx = get_curr_cor_idx();
+    void * ret = (void *)(uintptr_t)(corstruct->corpointers)[curidx].outaddr;
+    return ret;
+}
+
+//frees stack , changes idx and stuff , manages yield array , switches context
+void cor_kill(cor_struct * corstruct){                                      
     int number = get_cor_num();
     int curidx = get_curr_cor_idx();
-    free((void *)(uintptr_t)corstruct->corpointers[curidx].stackptr);
+
+    void* temp  =(void *)(uintptr_t)corstruct->corpointers[curidx].stackptr;
+    free(temp);
+    
     for(int i=curidx ; i<number-1 ; i++){
         corstruct->corpointers[i].rsp = corstruct->corpointers[i+1].rsp;
         corstruct->corpointers[i].rbp = corstruct->corpointers[i+1].rbp;
         corstruct->corpointers[i].rip = corstruct->corpointers[i+1].rip;
         corstruct->corpointers[i].stackptr = corstruct->corpointers[i+1].stackptr;
+        corstruct->corpointers[i].inaddr = corstruct->corpointers[i+1].inaddr;
+        corstruct->corpointers[i].outaddr = corstruct->corpointers[i+1].outaddr;
     }
 
     
@@ -67,14 +90,11 @@ void cor_kill(cor_struct * corstruct){
     corstruct->corpointers[number-1].rbp = 0;
     corstruct->corpointers[number-1].rip = 0;
     corstruct->corpointers[number-1].stackptr = 0;
-
-
-
-
+    corstruct->corpointers[number -1].inaddr = 0;
+    corstruct->corpointers[number -1].outaddr = 0;
     number -= 1;
     if (curidx >= number)  
         curidx = 0;
-
 
     set_cor_num((uint64_t)number);
     set_curr_cor_idx((uint64_t)curidx); 
@@ -82,6 +102,15 @@ void cor_kill(cor_struct * corstruct){
     return;
 }
 
+//idk..run it maybe ?
+void coroutine_run(cor_struct *corstruct){
+    while(1){
+        coroutine_yield(corstruct->corpointers);
+        int num = get_cor_num();
+        if(num <= 1)
+          break; 
+      }
+}
 
 
 
--- example.c
+++ example.c
@@ -3,10 +3,14 @@
 cor_struct * corstruct;     //declare global structure for coroutines
 
 void fun(){
-  for(int i=0; i<10; i++){
+  int yeh =  *(int *)(retrieve_args(corstruct));  //retrieve_args to get the input argument pointer
+  printf("input argument : %d\n" , yeh);          
+  for(int i=0; i<yeh; i++){
     printf("routine1: %d\n", i);
     coroutine_yield(corstruct->corpointers);
   }
+  int * ret = (int *)retcor_val(corstruct);     //rector_val to get the output argument pointer
+  *ret = 50;                                    //modifying the output
   cor_kill(corstruct);  
   return;
 }
@@ -16,6 +20,7 @@ void fun1(){
     printf("routine2: %d\n", i);
     coroutine_yield(corstruct->corpointers);
   }
+  
   cor_kill(corstruct);
   return;
 }
@@ -25,14 +30,13 @@ int main(){
   // sleep(20);
   corstruct = cor_struct_init();
   coroutines_init(&(corstruct->corpointers)[get_cor_num()]);
-  register_coroutines(fun , corstruct);
-  register_coroutines(fun1 , corstruct);
-  while(1){
-    coroutine_yield(corstruct->corpointers);
-    int num = get_cor_num();
-    if(num <= 1)
-      break; 
-  }
+  int temp = 20;
+  int ret = 0;
+  register_coroutines(fun , corstruct ,&temp , &ret);
+  register_coroutines(fun1 , corstruct , NULL, NULL);
+
+  coroutine_run(corstruct);    //jus run it
+  printf("return from fun is: %d\n", ret);
 
 }
 
